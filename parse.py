a79e08e79ebca935d8658158c45975cf29e85930
Andrew Epstein <aepstein@udel.edu>
2016-05-26 15:26:25 -0400

    Proper handling of A-Z as numbers, more error checking

--- parse.py
+++ parse.py
@@ -26,9 +26,12 @@ def applyOp(string, op, param1=None, param2=None):
     elif op == 't': # Toggle the case of all characters in word
         return ''.join(c.lower() if c.isupper() else c.upper() for c in string)
     elif op == 'T': # Toggle the case of characters at position N
-        param1 = int(param1, base=16)
-        c = string[param1]
-        return string[:param1] + (c.lower() if c.isupper() else c.upper()) + string[param1 + 1:]
+        param1 = int(param1, base=36)
+        try:
+            c = string[param1]
+            return string[:param1] + (c.lower() if c.isupper() else c.upper()) + string[param1 + 1:]
+        except:
+            return string
     elif op == 'r': # Reverse the entire word
         return string[::-1]
     elif op == 'd': # Duplicate the entire word
@@ -51,26 +54,26 @@ def applyOp(string, op, param1=None, param2=None):
     elif op == ']': # Deletes last character
         return string[:-1]
     elif op == 'D': # Deletes character at position N
-        param1 = int(param1, base=16)
+        param1 = int(param1, base=36)
         return string[:param1] + string[param1 + 1:]
     elif op == 'x': # Extracts M characters, starting at position N
-        N = int(param1, base=16)
-        M = int(param1, base=16)
+        N = int(param1, base=36)
+        M = int(param2, base=36)
         if len(string) <= N:
             return string
         return string[N: N + M]
     elif op == 'O': # Deletes M characters, starting at position N
-        param1 = int(param1, base=16)
-        param2 = int(param2, base=16)
+        param1 = int(param1, base=36)
+        param2 = int(param2, base=36)
         return string[:param1] + string[param1 + param2:]
     elif op == 'i': # Inserts character X at position N
-        param1 = int(param1, base=16)
+        param1 = int(param1, base=36)
         return string[:param1] + param2 + string[param1:]
     elif op == 'o': # Overwrites character at position N with X
-        param1 = int(param1, base=16)
+        param1 = int(param1, base=36)
         return string[:param1] + param2 + string[param1 + 1:]
     elif op == '\'': # Truncate word at position N
-        param1 = int(param1)
+        param1 = int(param1, base=36)
         return string[:param1]
     elif op == 's': # Replace all instances of X with Y
         return ''.join(param2 if c == param1 else c for c in string)
@@ -78,10 +81,16 @@ def applyOp(string, op, param1=None, param2=None):
         return ''.join('' if c == param1 else c for c in string)
     elif op == 'z': # Duplicates first character N times
         param1 = int(param1)
-        return string[0] * param1 + string
+        try:
+            return string[0] * param1 + string
+        except:
+            return string
     elif op == 'Z': # Duplicates last character N times
         param1 = int(param1)
-        return string + string[-1] * param1
+        try:
+            return string + string[-1] * param1
+        except:
+            return string
     elif op == 'q': # Duplicate every character
         return ''.join(c * 2 for c in string)
     elif op == 'k': # Swaps first two characters
@@ -89,38 +98,59 @@ def applyOp(string, op, param1=None, param2=None):
     elif op == 'K': # Swaps last two characters
         return string[:-2] + string[-1] + string[-2]
     elif op == '*': # Swaps character at position X with character at position Y
-        param1 = int(param1, base=16)
-        param2 = int(param2, base=16)
+        param1 = int(param1, base=36)
+        param2 = int(param2, base=36)
         l = list(string)
-        l[param1], l[param2] = l[param2], l[param1]
-        return ''.join(l)
+        try:
+            l[param1], l[param2] = l[param2], l[param1]
+            return ''.join(l)
+        except:
+            return string
     elif op == 'L': # Bitwise shift left character at position N
-        param1 = int(param1, base=16)
-        print string[:param1] + chr(ord(string[param1]) << 1) + string[param1 + 1:]
+        param1 = int(param1, base=36)
+        try:
+            return string[:param1] + chr(ord(string[param1]) << 1) + string[param1 + 1:]
+        except:
+            return string
     elif op == 'R': # Bitwise shift right character at position N
-        param1 = int(param1, base=16)
-        return string[:param1] + chr(ord(string[param1]) >> 1) + string[param1 + 1:]
+        param1 = int(param1, base=36)
+        try:
+            return string[:param1] + chr(ord(string[param1]) >> 1) + string[param1 + 1:]
+        except:
+            return string
     elif op == '+': # Increment character at position N by 1 ascii value
-        param1 = int(param1, base=16)
-        return string[:param1] + chr(ord(string[param1]) + 1) + string[param1 + 1:]
+        param1 = int(param1, base=36)
+        try:
+            return string[:param1] + chr(ord(string[param1]) + 1) + string[param1 + 1:]
+        except:
+            return string
     elif op == '-': # Decrement character at position N by 1 ascii value
-        param1 = int(param1, base=16)
-        return string[:param1] + chr(ord(string[param1]) - 1) + string[param1 + 1:]
+        param1 = int(param1, base=36)
+        try:
+            return string[:param1] + chr(ord(string[param1]) - 1) + string[param1 + 1:]
+        except:
+            return string
     elif op == '.': # Replaces character at position N with value at N plus 1
-        param1 = int(param1, base=16)
+        param1 = int(param1, base=36)
         l = list(string)
-        l[param1] = l[param1 + 1]
-        return ''.join(l)
+        try:
+            l[param1] = l[param1 + 1]
+            return ''.join(l)
+        except:
+            return string
     elif op == ',': # Replaces character at position N with value at N minus 1
-        param1 = int(param1, base=16)
+        param1 = int(param1, base=36)
         l = list(string)
-        l[param1] = l[param1 - 1]
-        return ''.join(l)
+        try:
+            l[param1] = l[param1 - 1]
+            return ''.join(l)
+        except:
+            return string
     elif op == 'y': # Duplicates first N characters
-        param1 = int(param1, base=16)
+        param1 = int(param1, base=36)
         return string[:param1] + string
     elif op == 'Y': # Duplicates last N characters
-        param1 = int(param1, base=16)
+        param1 = int(param1, base=36)
         return string + string[-param1:]
     elif op == 'E': # Lowercase the whole line, then uppercase the first letter and every letter after a space
         return ' '.join(c.capitalize() for c in string.lower().split(' '))
@@ -181,10 +211,10 @@ assert(applyOp(s, 'u') == 'P@SSW0RD')
 assert(applyOp(s, 'c') == 'P@ssw0rd')
 assert(applyOp(s, 'C') == 'p@SSW0RD')
 assert(applyOp(s, 't') == 'P@SSw0RD')
-assert(applyOp(s, 'T', 3) == 'p@sSW0rd')
+assert(applyOp(s, 'T', '3') == 'p@sSW0rd')
 assert(applyOp(s, 'r') == 'dr0Wss@p')
 assert(applyOp(s, 'd') == 'p@ssW0rdp@ssW0rd')
-assert(applyOp(s, 'p', 2) == 'p@ssW0rdp@ssW0rdp@ssW0rd')
+assert(applyOp(s, 'p', '2') == 'p@ssW0rdp@ssW0rdp@ssW0rd')
 assert(applyOp(s, 'f') == 'p@ssW0rddr0Wss@p')
 assert(applyOp(s, '{') == '@ssW0rdp')
 assert(applyOp(s, '}') == 'dp@ssW0r')
@@ -192,26 +222,26 @@ assert(applyOp(s, '$', '1') == 'p@ssW0rd1')
 assert(applyOp(s, '^', '1') == '1p@ssW0rd')
 assert(applyOp(s, '[') == '@ssW0rd')
 assert(applyOp(s, ']') == 'p@ssW0r')
-assert(applyOp(s, 'D', 3) == 'p@sW0rd')
-assert(applyOp(s, 'x', 0, 4) == 'p@ss')
-assert(applyOp(s, 'O', 1, 2) == 'psW0rd')
-assert(applyOp(s, 'i', 4, '!') == 'p@ss!W0rd')
-assert(applyOp(s, 'o', 3, '$') == 'p@s$W0rd')
-assert(applyOp(s, '\'', 6) == 'p@ssW0')
+assert(applyOp(s, 'D', '3') == 'p@sW0rd')
+assert(applyOp(s, 'x', '0', '4') == 'p@ss')
+assert(applyOp(s, 'O', '1', '2') == 'psW0rd')
+assert(applyOp(s, 'i', '4', '!') == 'p@ss!W0rd')
+assert(applyOp(s, 'o', '3', '$') == 'p@s$W0rd')
+assert(applyOp(s, '\'', '6') == 'p@ssW0')
 assert(applyOp(s, 's', 's', '$') == 'p@$$W0rd')
 assert(applyOp(s, '@', 's') == 'p@W0rd')
-assert(applyOp(s, 'z', 2) == 'ppp@ssW0rd')
-assert(applyOp(s, 'Z', 2) == 'p@ssW0rddd')
+assert(applyOp(s, 'z', '2') == 'ppp@ssW0rd')
+assert(applyOp(s, 'Z', '2') == 'p@ssW0rddd')
 assert(applyOp(s, 'q') == 'pp@@ssssWW00rrdd')
 assert(applyOp(s, 'k') == '@pssW0rd')
 assert(applyOp(s, 'K') == 'p@ssW0dr')
-assert(applyOp(s, '*', 3, 4) == 'p@sWs0rd')
+assert(applyOp(s, '*', '3', '4') == 'p@sWs0rd')
 # assert(applyOp(s, 'L', 2) == 'p@æsW0rd')
-assert(applyOp(s, 'R', 2) == 'p@9sW0rd')
-assert(applyOp(s, '+', 2) == 'p@tsW0rd')
-assert(applyOp(s, '-', 1) == 'p?ssW0rd')
-assert(applyOp(s, '.', 1) == 'psssW0rd')
-assert(applyOp(s, ',', 1) == 'ppssW0rd')
-assert(applyOp(s, 'y', 2) == 'p@p@ssW0rd')
-assert(applyOp(s, 'Y', 2) == 'p@ssW0rdrd')
+assert(applyOp(s, 'R', '2') == 'p@9sW0rd')
+assert(applyOp(s, '+', '2') == 'p@tsW0rd')
+assert(applyOp(s, '-', '1') == 'p?ssW0rd')
+assert(applyOp(s, '.', '1') == 'psssW0rd')
+assert(applyOp(s, ',', '1') == 'ppssW0rd')
+assert(applyOp(s, 'y', '2') == 'p@p@ssW0rd')
+assert(applyOp(s, 'Y', '2') == 'p@ssW0rdrd')
 assert(applyOp(e, 'E') == 'P@ssw0rd W0rld')
